2fefffa400b5a32e2644abd40682946da97f37e9
louis.tychon1@gmail.com <louis.tychon1@gmail.com>
2022-09-15 16:54:00 +0200

    all joueurs page

--- resources/views/pages/equipe/show.blade.php
+++ resources/views/pages/equipe/show.blade.php
@@ -20,7 +20,7 @@
 
     <div class="m-4 p-4 flex flex-wrap justify-evenly">
         @foreach ($joueurs as $joueur)
-            <div class="flex">
+            <div class="flex mb-4">
                 <a
                     class="block mt-4 p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
                     <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
--- resources/views/pages/joueur/index.blade.php
+++ resources/views/pages/joueur/index.blade.php
@@ -1,4 +1,24 @@
 @extends("layout.index")
 @section("content")
-<p>Hello world !</p>
+<h1
+    class="m-4 p-4 text-2xl font-extrabold tracking-tight leading-none text-black-500">
+        Tous les joueurs </h1>
+    <div class="m-4 p-4 flex flex-wrap justify-evenly">
+        @foreach ($alljoueurs as $joueur)
+        <div class="flex">
+            <a href="/joueurs/{{ $joueur->id }}/show"
+                class="block mt-4 p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"> Nom :
+                    {{ $joueur->nom }}</h5>
+                <ul class="text-white">
+                    <li>->{{ $joueur->pays }}</li>
+                    <li>->{{ $joueur->age }}</li>
+                </ul>
+            </a>
+        </div>
+        @endforeach
+    </div>
+
+    <a href="{{Route('equipescreate')}}"><button type="button"
+        class="block m-auto focus:outline-none text-white bg-purple-700 hover:bg-red-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900">Créer une équipe</button></a>
 @endsection
