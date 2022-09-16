89aa86ec8c538859ba09f162db0ca6e2cdadf43e
louistychon <louis.tychon1@gmail.com>
2022-09-16 23:01:33 +0200

    correction route page alljoueurs

--- resources/views/pages/joueur/index.blade.php
+++ resources/views/pages/joueur/index.blade.php
@@ -3,7 +3,7 @@
 <h1
     class="m-4 p-4 text-2xl font-extrabold tracking-tight leading-none text-black-500">
         Tous les joueurs </h1>
-    <div class="m-4 p-4 grid grid-cols-4 justify-evenly">
+    <div class="m-4 p-4 grid grid-cols-4 gap-3 justify-evenly">
         @foreach ($alljoueurs as $joueur)
         <div class="flex flex-col block mt-4 p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
             <form method="post" action="/joueurs/{{ $joueur->id }}/delete">
@@ -25,6 +25,6 @@
         @endforeach
     </div>
 
-    <a href="{{Route('equipescreate')}}"><button type="button"
-        class="block m-auto focus:outline-none text-white bg-purple-700 hover:bg-red-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900">Créer une équipe</button></a>
+    <a href="{{Route('joueurscreate')}}"><button type="button"
+        class="block m-auto focus:outline-none text-white bg-purple-700 hover:bg-red-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900">Créer un joueur</button></a>
 @endsection
